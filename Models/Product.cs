c9fcc0f3c54a625bb133398be5b302728934485f
Diwas Poudel <diwaspoudel.dev@gmail.com>
2026-01-30 09:46:48 -0500

    Child Parent Component and Routing Related

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,8 +2,10 @@
 {
     public class Product
     {
+        public int Id { get; set; }
         public decimal Price { get; set; }
         public string? Name { get; set; }
         public bool IsActive { get; set; }
+        public List<ProductProperty> ProductLists { get; set; } = new();
     }
 }
--- Models/Product_Prop.cs
+++ Models/Product_Prop.cs
@@ -1,6 +1,10 @@
 ﻿namespace BlazorBasics.Models
 {
-    public class Product_Prop
+    public class ProductProperty
     {
+        public int Id { get; set; }
+        public string? Key { get; set; }
+        public string? Value { get; set; }
+
     }
 }
