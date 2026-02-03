6122c60afb48d194c7abb08c3d464d312d11cc40
Diwas Poudel <diwaspoudel.dev@gmail.com>
2026-02-03 10:20:22 -0500

    Project Basics Setup

---
+++ Helpers/IJSRuntimeExceptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorBasics.Helpers
+{
+    public static class IJSRuntimeExceptions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
